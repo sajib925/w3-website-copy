b9f65538119478221f10cd6a2d155e4c766eb232
sajib925 <sajibahmed1004@gmail.com>
2023-12-20 01:30:50 +0600

    some fixed

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -10,7 +10,7 @@ export const Hero = () => {
                 <div className="max-w-screen-2xl w-full mx-auto px-4 h-full">
                     <div className="flex items-center justify-center gap-[30px] flex-col h-full">
                         <h1 className="text-white text-[100px] font-bold">Learn to Code</h1>
-                        <h3 className="text-2xl text-[#FFF4A3] font-bold">With the world's largest web developer site.</h3>
+                        <h3 className="text-2xl text-[#FFF4A3] font-bold">{"With the world's largest web developer site."}</h3>
                         <div className="max-w-[500px] w-full flex items-center justify-center overflow-hidden rounded-[25px]">
                             <input placeholder="Search our tutorials e.g. HTMLSection" className="py-[10px] pr-[10px] pl-[28px] w-[75%] outline-none"/>
                             <div className='text-white p-[10px] bg-[#04AA6D] w-[25%] flex items-center justify-center cursor-pointer'>
